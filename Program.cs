126c6cbd34a13b08807c88ab9be728f7e11d7c1b
Enisksz <71214539+Enisksz@users.noreply.github.com>
2022-03-09 11:25:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiziTanımlamaErişim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" };
+
+            int[] dizi;
+            dizi = new int[5];
+
+            //Dizilere değer atama ve erişme.
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(renkler[0]);
+            Console.WriteLine(dizi[3]);
+
+            //Döngülerle Dizi Kullanımı
+            //Klavyeden girilen n tane sayının ortalamasını hesaplayan program.
+
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz: ");
+            int diziUzunlugu =int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.WriteLine("Lütfen {0}. sayıyı giriniz.",i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+            }
+            Console.WriteLine("ortalama: "+toplam/diziUzunlugu);
+
+        }
+    }
+}
